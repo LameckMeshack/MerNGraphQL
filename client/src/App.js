19e67f479bd29f643e1f728b7ae6e497ca77d341
RhinolamerSonwar <rhinolamer@gmail.com>
2022-06-22 17:14:35 +0300

    client deletion and appolo cache

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Clients } from "./components/Clients";
 import { Header } from "./components/Header";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 function App() {
   return (
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,29 @@
+import { useMutation } from "@apollo/client";
 import { FaTrash } from "react-icons/fa";
+import { GET_CLIENTS } from "../queries/ClientQueries";
+import { DELETE_CLIENT } from "./mutations/clientMutations";
+import { Spinner } from "./Spinner";
 
 export const ClientRow = ({ client }) => {
+  const [deleteClient, { loading }] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter((c) => c.id !== deleteClient.id) },
+      });
+    },
+  });
+  if (loading) return <Spinner />;
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
