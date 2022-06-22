e01f2e400a4d1a8d5015244a955d55dd62922dbc
RhinolamerSonwar <rhinolamer@gmail.com>
2022-06-22 12:26:55 +0300

    header added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
